dc7c474011c238a64db412485c5d71fb76746b58
Pranav Srivastava <128836534+ps2956@users.noreply.github.com>
2025-01-29 18:36:53 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,69 +1,111 @@
 <?php 
-$q=mysqli_query($conn,"select * from contact");
-$r=mysqli_num_rows($q);
-if($r==false)
+include('dbconfig.php');
+extract($_POST);
+if(isset($save))
 {
-echo "<h3 style='color:Red'>No any records found ! </h3>";
+
+mysqli_query($conn,"insert into contact values('','$n','$m','$e','$msg',now())");
+	
+$err="<font color='green'>Admin Will Contact you soon</font>";	
+
 }
-else
-{
+
 ?>
 
-<script type="text/javascript">
-function deletes(id)
-{
-	a=confirm('Are You Sure To Remove This Record ?')
-	 if(a)
-     {
-        window.location.href='delete_contact.php?id='+id;
-     }
-}
-</script>	
+        <div class="container">
 
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                
+                <ol class="breadcrumb">
+                    
+                    </li>
+                   
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
 
-<div class="row">
-	<div class="col-sm-12" style="color:orange;">
-		<h1 align="center" >Contact Us</h1>
-	</div>
-</div>
-<div class="row">
+        <!-- Content Row -->
+       <div class="row">
+            <div class="col-md-8">
+                <h3><font color="#00FF33">Contact us </font></h3>
+                <form name="sentMessage" method="post" id="contactForm" novalidate>
+                  <?php echo @$err; ?>
+				    <div class="control-group form-group">
+                        
+						
+						<div class="controls">
+                            <label>Name:</label>
+                            <input type="text" class="form-control" name="n" required data-validation-required-message="Please enter your name.">
+                            <p class="help-block"></p>
+                        </div>
+                    </div>
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label>Mobile Number:</label>
+                            <input type="number" class="form-control" name="m" required data-validation-required-message="Please enter your phone number.">
+                        </div>
+                    </div>
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label>Email Address:</label>
+                            <input type="email" class="form-control" name="e" required data-validation-required-message="Please enter your email address.">
+                        </div>
+                    </div>
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label>Message:</label>
+                            <textarea rows="10" name="msg" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+                        </div>
+                    </div>
+                    <div id="success"></div>
+                    <!-- For success/fail messages -->
+                    <button type="submit" name="save" class="btn btn-primary">Send Message</button>
+					<h1></h1>
+                </form>
+            </div>
+			<div class="col-md-4" style="margin-top:30px">
+                <h3>Contact Details</h3>
+                <p>
+                    E-20 Sector 63<br>Noida, India 90210<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: (123) 456-7890</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">E</abbr>: <a href="mailto:name@example.com">name@example.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Sunday: 9:00 AM to 9:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                    <li>
+                        <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
-<div class="col-sm-12">
+        </div>
+        
+            <!-- Contact Details Column -->
+            
+        <!-- /.row -->
 
-<table class="table table-bordered">
+        <!-- Contact Form -->
+        <!-- In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
+        <!-- /.row -->
 
-	<thead >
-	
-	<tr class="success">
-		<th>Sr.No</th>
-		<th>User Name</th>
-		<th>Email</th>
-		<th>Mobile</th>
-		<th>Message</th>
-		<th>Delete</th>
-		</tr>
-		</thead>
-		
-		<?php
-		$i=1;
-	while($row=mysqli_fetch_assoc($q))
-		{
-			echo "<tr>";
-			echo "<td>".$i."</td>";
-			echo "<td>".$row['name']."</td>";
-			echo "<td>".$row['mobile']."</td>";
-			echo "<td>".$row['email']."</td>";
-			echo "<td>".$row['message']."</td>";
-			echo "<td><a href='#' onclick='deletes($row[id])'>Delete</a></td>";
-			echo "</tr>";
-		$i++;
-		}
-		
-		?>
-		
-		
-		
-</table>
-</div>
-</div>
-<?php }?>
+    <br/><br/>
+    
+    </div>
+    <!-- /.container -->
